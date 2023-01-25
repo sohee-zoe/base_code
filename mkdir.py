b0fcd4aedf13a6cc5860d61540eed9bf20dae0a9
Sohee <sohee.forest@gmail.com>
2023-01-25 14:52:24 +0900

    Update mkdir.py

--- mkdir.py
+++ mkdir.py
@@ -2,8 +2,8 @@ import os
  
 def mkdir(directory):
     try:
-        if not os.path.exists(directory):
-            os.makedirs(directory)
+        if not os.path.exists(directory): # 디렉토리 존재 여부확인
+            os.makedirs(directory)        # 모든 하위 폴더까지 생성
     except OSError:
         print ('Error: Creating directory. ' +  directory)
  
