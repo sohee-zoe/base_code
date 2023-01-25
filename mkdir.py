d767076a2e20c45bc8a1bcad0a9ea67634c53e08
Sohee <sohee.forest@gmail.com>
2023-01-25 15:02:55 +0900

    Update mkdir.py

--- mkdir.py
+++ mkdir.py
@@ -8,3 +8,9 @@ def mkdir(directory):
         print ('Error: Creating directory. ' +  directory)
  
 # createFolder('/Users/aaron/Desktop/test')
+
+from pathlib import Path
+dir_path = Path("")
+# file_path = Path("")
+# dir_path.exists()     # 존재 여부 확인
+dir_path.mkdir(exist_ok=True, parents=True) # 없으면 생성
